aa018686d50ee62fb51806dbe3ab21789e51833b
Viet Ta <89030236+viet1012@users.noreply.github.com>
2025-09-29 08:19:31 +0700

    init project

---
+++ src/main/java/com/example/factory_utility_visualization_be/FactoryUtilityVisualizationBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.factory_utility_visualization_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryUtilityVisualizationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryUtilityVisualizationBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/factory_utility_visualization_be/FactoryUtilityVisualizationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.factory_utility_visualization_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryUtilityVisualizationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
