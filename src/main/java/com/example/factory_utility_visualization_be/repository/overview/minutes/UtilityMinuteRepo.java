224fa8559d03d1e36aa99f35d4a1210b2e503a31
Viet Ta <89030236+viet1012@users.noreply.github.com>
2026-07-02 14:44:29 +0700

    update minutes chart

--- src/main/java/com/example/factory_utility_visualization_be/repository/overview/minutes/UtilityMinuteRepo.java
+++ src/main/java/com/example/factory_utility_visualization_be/repository/overview/minutes/UtilityMinuteRepo.java
@@ -170,141 +170,113 @@ import java.util.List;
 public interface UtilityMinuteRepo extends JpaRepository<DummyEntity, Long> {
 
 	@Query(value = """
-			WITH CleanData AS (
-			    SELECT *
-			    FROM F2_Utility_Para_History
-			    WHERE recorded_at > DATEADD(MINUTE, -:minutes, GETDATE())
-			      AND [value] > 0
-			),
-			Base AS (
-			    SELECT
-			        sc.fac,
-			        ch.cate,
-			        pa.name_en,
-			        pa.box_device_id,
-			        hi.plc_address,
-			        hi.[value],
-			        hi.recorded_at,
-			
-			        DATEADD(MINUTE, DATEDIFF(MINUTE, 0, hi.recorded_at), 0) AS minute_time,
-			
-			        hi.[value] - LAG(hi.[value]) OVER (
-			            PARTITION BY hi.box_device_id, hi.plc_address
-			            ORDER BY hi.recorded_at
-			        ) AS value_per_minute
-			
-			    FROM CleanData hi
-			
-			    INNER JOIN F2_Utility_Para pa
-			        ON hi.box_device_id = pa.box_device_id
-			       AND hi.plc_address  = pa.plc_address
-			
-			    INNER JOIN F2_Utility_Scada_Channel ch
-			        ON hi.box_device_id = ch.box_device_id
-			
-			    INNER JOIN F2_Utility_Scada sc
-			        ON ch.scada_id = sc.scada_id
-			
-			    WHERE (:fac = 'KVH' OR sc.fac = :fac)
-			      AND (
-			            (:type = 'ELECTRICITY'
-			                AND pa.name_en = 'Total Energy Consumption')
-			
-			         OR (:type = 'WATER'
-			                AND pa.name_en LIKE '%Cooling tank%')
-			
-			         OR (:type = 'AIR'
-			                AND pa.name_en = 'Sensor compressed air pressure Data')
-			      )
-			)
-			
-			SELECT
-			    minute_time AS ts,
-			    SUM(value_per_minute) AS value,
-			    'ELECTRICITY' AS name
-			FROM Base
-			WHERE :type = 'ELECTRICITY'
-			  AND value_per_minute IS NOT NULL
-			  AND value_per_minute >= 0
-			GROUP BY
-			    minute_time
-			
-			UNION ALL
-			
-			SELECT
-			    minute_time AS ts,
-			    [value] AS value,
-			    CONCAT(
-			        fac,
-			        ' - ',
-			        LTRIM(RTRIM(
-			            REPLACE(
-			                REPLACE(
-			                    REPLACE(name_en, '_PT100', ''),
-			                    'Cooling tank ', 'Tank '
-			                ),
-			                'temperature data', ''
-			            )
-			        ))
-			    ) AS name
-			FROM (
-			    SELECT
-			        minute_time,
-			        fac,
-			        name_en,
-			        [value],
-			        ROW_NUMBER() OVER (
-			            PARTITION BY minute_time, fac
-			            ORDER BY [value] DESC
-			        ) AS rn
-			    FROM Base
-			    WHERE :type = 'WATER'
-			      AND :fac = 'KVH'
-			) x
-			WHERE rn = 1
-			
-			UNION ALL
-			
-			SELECT
-			    minute_time AS ts,
-			    MAX([value]) AS value,
-			    CONCAT(
-			        fac,
-			        ' - ',
-			        LTRIM(RTRIM(
-			            REPLACE(
-			                REPLACE(
-			                    REPLACE(name_en, '_PT100', ''),
-			                    'Cooling tank ', 'Tank '
-			                ),
-			                'temperature data', ''
-			            )
-			        ))
-			    ) AS name
-			FROM Base
-			WHERE :type = 'WATER'
-			  AND :fac <> 'KVH'
-			GROUP BY
-			    minute_time,
-			    fac,
-			    box_device_id,
-			    name_en
-			
-			UNION ALL
-			
-			SELECT
-			    minute_time AS ts,
-			    AVG([value]) AS value,
-			    'AIR' AS name
-			FROM Base
-			WHERE :type = 'AIR'
-			GROUP BY
-			    minute_time
-			
-			ORDER BY
-			    ts,
-			    name
-			""", nativeQuery = true)
+        WITH CleanData AS (
+            SELECT *
+            FROM F2_Utility_Para_History
+            WHERE recorded_at > DATEADD(MINUTE, -:minutes, GETDATE())
+              AND [value] > 0
+        ),
+        Base AS (
+            SELECT
+                sc.fac,
+                ch.cate,
+                pa.name_en,
+                pa.box_device_id,
+                hi.plc_address,
+                hi.[value],
+                hi.recorded_at,
+
+                DATEADD(MINUTE, DATEDIFF(MINUTE, 0, hi.recorded_at), 0) AS minute_time,
+
+                hi.[value] - LAG(hi.[value]) OVER (
+                    PARTITION BY hi.box_device_id, hi.plc_address
+                    ORDER BY hi.recorded_at
+                ) AS value_per_minute
+
+            FROM CleanData hi
+
+            INNER JOIN F2_Utility_Para pa
+                ON hi.box_device_id = pa.box_device_id
+               AND hi.plc_address  = pa.plc_address
+
+            INNER JOIN F2_Utility_Scada_Channel ch
+                ON hi.box_device_id = ch.box_device_id
+
+            INNER JOIN F2_Utility_Scada sc
+                ON ch.scada_id = sc.scada_id
+
+            WHERE (:fac = 'KVH' OR sc.fac = :fac)
+              AND (
+                    (:type = 'ELECTRICITY'
+                        AND pa.name_en = 'Total Energy Consumption')
+
+                 OR (:type = 'WATER'
+                        AND pa.name_en LIKE '%Cooling tank%')
+
+                 OR (:type = 'AIR'
+                        AND pa.name_en = 'Sensor compressed air pressure Data')
+              )
+        )
+
+        SELECT
+            minute_time AS ts,
+            SUM(value_per_minute) AS value,
+            'ELECTRICITY' AS name
+        FROM Base
+        WHERE :type = 'ELECTRICITY'
+          AND value_per_minute IS NOT NULL
+          AND value_per_minute >= 0
+        GROUP BY
+            minute_time
+
+        UNION ALL
+
+        SELECT
+            minute_time AS ts,
+            [value] AS value,
+            CONCAT(
+                fac,
+                ' - ',
+                LTRIM(RTRIM(
+                    REPLACE(
+                        REPLACE(name_en, 'Cooling tank ', 'Tank '),
+                        ' temperature data', ''
+                    )
+                ))
+            ) AS name
+        FROM (
+            SELECT
+                minute_time,
+                fac,
+                name_en,
+                [value],
+                ROW_NUMBER() OVER (
+                    PARTITION BY minute_time, fac
+                    ORDER BY [value] DESC
+                ) AS rn_fac
+            FROM Base
+            WHERE :type = 'WATER'
+        ) x
+        WHERE
+            (:fac = 'KVH' AND rn_fac = 1)
+            OR
+            (:fac <> 'KVH')
+
+        UNION ALL
+
+        SELECT
+            minute_time AS ts,
+            AVG([value]) AS value,
+            'AIR' AS name
+        FROM Base
+        WHERE :type = 'AIR'
+        GROUP BY
+            minute_time
+
+        ORDER BY
+            ts,
+            name
+        """, nativeQuery = true)
 	List<Object[]> findUtilityDeltaPerMinute(
 			@Param("fac") String fac,
 			@Param("minutes") int minutes,
